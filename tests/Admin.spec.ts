8c878b9fa7f1292ac95f3634d265287749bf3dc4
Nguyen Duc An <nguyenducan03.tm@gmail.com>
2026-09-19 15:32:06 +0700

    1111

--- tests/Admin.spec.ts
+++ tests/Admin.spec.ts
@@ -1,6 +1,6 @@
 import {expect, test } from '../Libary/fixture/index';
 import { LinkURL, Account } from '../Data/account';
-
+import employee from '../Data/addEmployee.json'
 
 
 test.describe('Login and forget password', () => {
@@ -30,3 +30,34 @@ test('Go to  Admin', async ({ loginHelper, choseMenuHelper , mainMenuPage}) => {
   await mainMenuPage.sidebar_collapse_btn.click();
   });
 });
+test.describe('Login and forget password', () => {
+  test.beforeEach(async ({ page , getLinkHelper }) => {
+    await page.route('**/*', (route) => {
+      const url = route.request().url();
+
+      if (
+        url.includes('googlesyndication') ||
+        url.includes('doubleclick') ||
+        url.includes('googleads')
+      ) {
+        route.abort();
+      } else {
+        route.continue();
+      }
+    });
+     await getLinkHelper.goto_URL();
+     await expect(page).toHaveURL(LinkURL);
+  });
+  for (const user of employee.add_user_data){
+  test(`${user.id} - ${user.description}`, async ({ loginHelper, choseMenuHelper , mainMenuPage}) => {
+  await loginHelper.login(Account[0].username , Account[0].password);
+  
+  await choseMenuHelper.chooseAdmin();
+  await expect(mainMenuPage.header_admin).toBeVisible();
+  await mainMenuPage.sidebar_collapse_btn.click();
+
+    
+  });
+  }
+});
+
